1b59cfd8b65e6b9511fec73b56d391f96f975bb9
m5cott <mcscottbsn@outlook.com>
2020-11-02 10:18:55 -0600

    checked for sudo privilege, added seperate applications file to clean up install script

--- ubuntu-install/ubuntu-install.sh
+++ ubuntu-install/ubuntu-install.sh
@@ -7,57 +7,60 @@
 # License: MIT License
 #
 
+# Global Variables
+priv="sudo"
+file="applications"
+
 function install {
     which $1 &> /dev/null
 
     if [ $? -ne 0 ]; then
         echo "Installing: ${1}..."
-        apt install $1
+        $priv apt install $1
     else
         echo "Already installed: ${1}"
     fi
 }
 
-# Deb Packages
-install curl
-install gnome-tweaks
-install git
-install git-core
-install build-essential
-install unzip
-install unrar
-install vim 
-install pylint
-install python-pip-whl
-install ffmpeg 
-install qemu 
-install qemu-utils 
-install qemu-kvm 
-install libvirt-clients 
-install libvirt-daemon-system 
-install bridge-utils
-install deluge
+# Check if user is sudo
+echo "Please enter your password to continue"
+[ "$UID" -eq 0 ] || exec $priv "$0" "$@"
 
-sudo usermod -aG kvm $USER
-sudo usermod -aG libvirt $USER
-sudo usermod -aG libvirt-qemu $USER
-sudo usermod -aG libvirt-dnsmasq $USER
+# Installing Packages from Main Repo
+while IFS= read -r line
+do
+    install "$line"
+done <"$file"
+
+# Adding VM groups to current user
+$priv usermod -aG kvm $USER
+$priv usermod -aG libvirt $USER
+$priv usermod -aG libvirt-qemu $USER
+$priv usermod -aG libvirt-dnsmasq $USER
 
 # Snaps
-sudo snap install qemu-virgil --edge
-sudo snap connect qemu-virgil:audio-record
-sudo snap connect qemu-virgil:kvm
-sudo snap connect qemu-virgil:raw-usb
-sudo snap connect qemu-virgil:removable-media
+$priv snap install qemu-virgil --edge
+$priv snap connect qemu-virgil:audio-record
+$priv snap connect qemu-virgil:kvm
+$priv snap connect qemu-virgil:raw-usb
+$priv snap connect qemu-virgil:removable-media
 
 # Quickemu
 git clone https://github.com/wimpysworld/quickemu.git
 
+# Pop Shell
+git clone https://github.com/pop-os/shell
+cd shell && make local-install
+
+# Cascadia Code PL (Work in Progress)
+wget https://github.com/microsoft/cascadia-code/releases/download/v2009.22/CascadiaCode-2009.22.zip
+unzip CascadiaCode-2009.22.zip -d CascadiaCode
+
 # VS Code
 wget -O code.deb https://go.microsoft.com/fwlink/?LinkID=760868
-sudo dpkg -i code.deb
+$priv dpkg -i code.deb
 rm -f code.deb
 
 # youtube-dl
-sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
-sudo chmod a+rx /usr/local/bin/youtube-dl
+$priv curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+$priv chmod a+rx /usr/local/bin/youtube-dl
