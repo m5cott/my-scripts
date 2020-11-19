07458af519013ee1c29c3b3a5098e17d72633e8c
m5cott <mcscottbsn@outlook.com>
2020-11-19 14:03:31 -0600

    initial commit

---
+++ shell/crlf2lf.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+###################################################
+ # Name: crlf2lf.sh
+ # Purpose: Windows eol to Unix/Linux eol
+ # Author: Michael Scott (m5cott)
+ # Created: 2020-11-19
+ # License: MIT License
+###################################################
+
+arg1=$1
+
+sed -i 's/\r//g' $1
